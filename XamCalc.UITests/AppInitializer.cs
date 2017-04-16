581a0839ca92d6d87dc0b530e741c92700f31214
Jesus Angulo <jesus.angulo@outlook.com>
2017-04-16 17:38:10 -0500

    Adding UI Tests

---
+++ XamCalc.UITests/AppInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.UITest;
+using Xamarin.UITest.Configuration;
+
+namespace XamCalc.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .InstalledApp("somostechies.calc")
+                    .StartApp(AppDataMode.Clear);
+            }
+
+            return ConfigureApp
+                .iOS
+                .InstalledApp("somostechies.calc")
+                .StartApp(AppDataMode.Clear); ;
+        }
+    }
+}
+
---
+++ XamCalc.UITests/Tests.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace XamCalc.UITests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+
+        [Test]
+        public void BasicSum()
+        {
+            //Wait for application start
+            app.WaitForElement(c => c.Marked("Display"));
+
+            //Enter a number
+            app.Tap(c => c.Marked("1"));
+            app.Tap(c => c.Marked("9"));
+
+            //Enter operation
+            app.Tap(c => c.Marked("+"));
+
+            //Enter a second number
+            app.Tap(c => c.Marked("5"));
+            app.Tap(c => c.Marked("4"));
+
+            //Get result
+            app.Tap(c=>c.Marked("="));
+
+            var result = app.Query(c => c.Marked("Display")).FirstOrDefault();
+            if (result == null)
+            {
+                Assert.Fail("No display was found");return;
+            }
+            var display = result.Text;
+            Assert.AreEqual("73",display,message:"Result was not as expected.");
+        }
+    }
+}
